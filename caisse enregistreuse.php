1e66ef644e0f3c8887aa8459db264a6e132d3d94
Edenforst <mick.ligier@hotmail.fr>
2023-10-13 11:55:57 +0200

    2eme version

--- caisse enregistreuse.php
+++ caisse enregistreuse.php
@@ -1,26 +1,10 @@
 <?php
 //Remplir tableau des valeurs de la monnaie et les associer a ce qui existe dans la caisse
 $caisse = ["500" => 1,"200" => 1,"100" => 0,"50" => 2,"20" => 3,"10" => 4,"5" => 10,"2" => 20,"1" => 4,"0.5" => 2,"0.2" => 20,"0.1" => 15,"0.05" => 23,"0.02" => 14,"0.01" => 30];
-$estvide = false;
-$Indice = 0;
-$ARendre = 50; //valeur que le client donne
-$monnayeur = [
-    "500" => 1,
-    "200" => 1,
-    "100" => 2,
-    "50" => 2,
-    "20" => 4,
-    "10" => 4,
-    "5" => 10,
-    "2" => 15,
-    "1" => 20,
-    "0,50" => 20,
-    "0,20" => 25, 
-    "0,10" => 30,
-    "0,05" => 30,
-    "0,02" => 30,
-    "0,01" => 30,
-];
+$estvide = false; // tant que la caisse n'est pas vide
+$Indice = 0; //pour se déplcare dans les tableaux asso
+$ARendre = 100; //monnaie que le client donne
+$monnayeur = ["500" => 1,"200" => 1,"100" => 2,"50" => 2,"20" => 4,"10" => 4,"5" => 10,"2" => 15,"1" => 20,"0,50" => 20,"0,20" => 25, "0,10" => 30,"0,05" => 30,"0,02" => 30,"0,01" => 30];
 
 while ($ARendre > 0 || !$estvide) { //tant qu'il reste de l'argent dans la caisse
     //si la valeur a rendre est plus élevé que la case actuelle
@@ -29,7 +13,7 @@ while ($ARendre > 0 || !$estvide) { //tant qu'il reste de l'argent dans la caiss
         //On rend la monnaie inférieur a celle qui est donné, et selon celle qui est disponible
         $ARendre = $ARendre - (int)$caisse[$Indice];
 
-        //on décrémente le compteur de la monnaie
+        //on décrémente le compteur de la caisse
         $caisse[$Indice] = $caisse[$Indice] - 1;
 
         //Sinon, on rend une monnaie plus petite
@@ -40,7 +24,7 @@ while ($ARendre > 0 || !$estvide) { //tant qu'il reste de l'argent dans la caiss
     $estvide = verifierCaisse($caisse);
 };
 
-echo "Voici votre monnaie :";
+echo "Voici votre monnaie :".$caisse;
 
 
 function verifierCaisse($caisse) {
@@ -58,5 +42,14 @@ function verifierCaisse($caisse) {
     //Sinon on retourne la valeur que la caisse est vide
     return true;
 };
+
+// function limit($caisse){ //fonction pour limiter la rente d'argent
+//     $caisse = ["500" => 1,"200" => 1,"100" => 0,"50" => 2,"20" => 3,"10" => 4,"5" => 10,"2" => 20,"1" => 4,"0.5" => 2,"0.2" => 20,"0.1" => 15,"0.05" => 23,"0.02" => 14,"0.01" => 30];
+//     $monnayeur = ["500" => 1,"200" => 1,"100" => 2,"50" => 2,"20" => 4,"10" => 4,"5" => 10,"2" => 15,"1" => 20,"0,50" => 20,"0,20" => 25, "0,10" => 30,"0,05" => 30,"0,02" => 30,"0,01" => 30];
+//     $combined_diff = 0;
+//     $keys_diff = array_diff_key($caisse, $monnayeur);
+//     $values_diff = array_diff_assoc($caisse, $monnayeur);
+//     $combined_diff = $keys_diff + $values_diff;
+// }
 
 ?>
